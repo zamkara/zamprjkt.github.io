561733d3395408866dd9a1980c8b29f4fb93a893
Kiko ☘ <70899024+nurzam891@users.noreply.github.com>
2021-05-27 23:20:34 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,21 +19,6 @@ linkRequest.onreadystatechange = function () {
 };
 linkRequest.send(null);
 
-workRequest.open("GET", "json/items.json", true);
-workRequest.onreadystatechange = function () {
-    if (workRequest.readyState === 4) {
-        if (workRequest.status === 200 || workRequest.status == 0) {
-            var html = "";
-            JSON.parse(workRequest.responseText).forEach(function (work) {
-                console.log(work.name);
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";
-            });
-            work.innerHTML = html;
-        }
-    }
-};
-workRequest.send(null);
-
 $(document).ready(function(){
     $(".preloader").delay(1000).fadeOut();
     })
